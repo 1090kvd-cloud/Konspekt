d5b93e68b7fd71984c6694212d4a016446010bbf
kvd <1090kvd@gmail.com>
2026-09-12 23:22:26 +0300

    Add files via upload

--- app/src/main/java/ru/vzvod/konspekt/ui/Components.kt
+++ app/src/main/java/ru/vzvod/konspekt/ui/Components.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -236,7 +237,7 @@ fun AppHeader(
             contentAlignment = Alignment.Center
         ) {
             Icon(
-                androidx.compose.material.icons.Icons.Filled.Menu,
+                Icons.Filled.Menu,
                 "Меню",
                 modifier = Modifier.size(26.dp),
                 tint = MaterialTheme.colorScheme.onSurfaceVariant
